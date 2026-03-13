d0d9079cb3fce060974877521cee25c0516f3284
vfedotova418-png <vfedotova418@gmail.com>
2026-03-13 13:17:41 +0300

    Step 7: arrays in JavaScript

--- main.js
+++ main.js
@@ -68,61 +68,110 @@
 //     console.log(str);
 // }
 
-function sum(a, b) {
-    return a + b;
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(3, 5));
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+// console.log(multiply(3, 5));
+
+// function sayHello(name) {
+//     console.log(`Hello, ${name}`);
+// }
+// sayHello("Тимофей");
+
+// function prinInfo(name, age) {
+//     console.log(`Имя: ${name}, возраст: ${age}`);
+// }
+// prinInfo("Helel", 18);
+
+// function greet(name = "Гость") {
+//     console.log("Привет, " + name);
+// }
+// greet();
+// greet("Анастасия");
+
+// function calculateDiscount(price, discount = 10) {
+//     return price - (price * (discount / 100))
+// }
+// console.log(calculateDiscount(2000, 15));
+
+// const add = function (a, b) {
+//     return a + b;
+// };
+// console.log(add(2, 3));
+
+// function makeCounter() {
+//     let count = 0;
+//     return function() {
+//         count++;
+//         return count;
+//     }
+// }
+
+// const counter = makeCounter();
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// const sumFunc = (a, b) => a + b;
+// const double = x => x * 2;
+// const sumFunc2 = (a, b) => a + b;
+// const sayGreeting = () => console.log("Hello");
+// const square = x => x * x;
+// const calculate = (a, b) => {
+//     let result = a + b;
+//     return result * 2;
+// }
+
+let numbersArr = [1, 2, 3, 4, 5];
+console.log(numbersArr);
+
+console.log(numbersArr[0]);
+console.log(numbersArr[1]);
+
+let colors = ["red", "green", "blue"];
+console.log(colors[0]);
+console.log(colors[2]);
+colors[1] = "yellow";
+console.log(colors);
+
+console.log(numbersArr.length);
+
+numbersArr.push(10);
+console.log(numbersArr);
+
+numbersArr.pop();
+console.log(numbersArr);
+
+let students = [];
+students.push("Helel", "Ray", "Kris");
+students.pop();
+console.log(students);
+
+let numbers2 = [10, 20, 30];
+for (let i = 0; i < numbers2.length; i++) {
+    console.log(numbers2[i]);
 }
-console.log(sum(3, 5));
 
-function multiply(a, b) {
-    return a * b;
-}
-console.log(multiply(3, 5));
-
-function sayHello(name) {
-    console.log(`Hello, ${name}`);
-}
-sayHello("Тимофей");
-
-function prinInfo(name, age) {
-    console.log(`Имя: ${name}, возраст: ${age}`);
-}
-prinInfo("Helel", 18);
-
-function greet(name = "Гость") {
-    console.log("Привет, " + name);
-}
-greet();
-greet("Анастасия");
-
-function calculateDiscount(price, discount = 10) {
-    return price - (price * (discount / 100))
-}
-console.log(calculateDiscount(2000, 15));
-
-const add = function (a, b) {
-    return a + b;
-};
-console.log(add(2, 3));
-
-function makeCounter() {
-    let count = 0;
-    return function() {
-        count++;
-        return count;
-    }
+for (let value of numbers2) {
+    console.log(value);
 }
 
-const counter = makeCounter();
-console.log(counter());
-console.log(counter());
-console.log(counter());
+let mixedArray = [1, "text", true, 3.14];
+console.log(mixedArray);
 
-const sumFunc = (a, b) => a + b;
-const double = x => x * 2;
-const sumFunc2 = (a, b) => a + b;
-const sayGreeting = () => console.log("Hello");
-const square = x => x * x;
-const calculate = (a, b) => {
-    let result = a + b;
-    return result * 2;
+console.log(numbersArr.indexOf(1));
+console.log(numbersArr.indexOf(2));
+
+console.log(numbersArr.includes(1));
+console.log(numbersArr.includes(42));
+
+let cities = ["Moscow", "Volgograd", "St.Peterburg"];
+let city = "Volgograd";
+if (cities.includes(city)) {
+    console.log(cities.indexOf(city));
 }
