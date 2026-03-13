c6c5c8420bd8da4fbb4e14d37be7e2af520d5d24
vfedotova418-png <vfedotova418@gmail.com>
2026-03-13 13:32:03 +0300

    Step 8: objects in JavaScript

--- main.js
+++ main.js
@@ -127,51 +127,117 @@
 //     return result * 2;
 // }
 
-let numbersArr = [1, 2, 3, 4, 5];
-console.log(numbersArr);
+// let numbersArr = [1, 2, 3, 4, 5];
+// console.log(numbersArr);
 
-console.log(numbersArr[0]);
-console.log(numbersArr[1]);
+// console.log(numbersArr[0]);
+// console.log(numbersArr[1]);
 
-let colors = ["red", "green", "blue"];
-console.log(colors[0]);
-console.log(colors[2]);
-colors[1] = "yellow";
-console.log(colors);
+// let colors = ["red", "green", "blue"];
+// console.log(colors[0]);
+// console.log(colors[2]);
+// colors[1] = "yellow";
+// console.log(colors);
 
-console.log(numbersArr.length);
+// console.log(numbersArr.length);
 
-numbersArr.push(10);
-console.log(numbersArr);
+// numbersArr.push(10);
+// console.log(numbersArr);
 
-numbersArr.pop();
-console.log(numbersArr);
+// numbersArr.pop();
+// console.log(numbersArr);
 
-let students = [];
-students.push("Helel", "Ray", "Kris");
-students.pop();
-console.log(students);
+// let students = [];
+// students.push("Helel", "Ray", "Kris");
+// students.pop();
+// console.log(students);
 
-let numbers2 = [10, 20, 30];
-for (let i = 0; i < numbers2.length; i++) {
-    console.log(numbers2[i]);
+// let numbers2 = [10, 20, 30];
+// for (let i = 0; i < numbers2.length; i++) {
+//     console.log(numbers2[i]);
+// }
+
+// for (let value of numbers2) {
+//     console.log(value);
+// }
+
+// let mixedArray = [1, "text", true, 3.14];
+// console.log(mixedArray);
+
+// console.log(numbersArr.indexOf(1));
+// console.log(numbersArr.indexOf(2));
+
+// console.log(numbersArr.includes(1));
+// console.log(numbersArr.includes(42));
+
+// let cities = ["Moscow", "Volgograd", "St.Peterburg"];
+// let city = "Volgograd";
+// if (cities.includes(city)) {
+//     console.log(cities.indexOf(city));
+// }
+
+let user2 = {
+    name: "Helel",
+    age: 18,
+    isStudent: true,
+    sayHello: function() {
+        console.log(`Hello, my name is ${user2.name}`);
+    },
+};
+user2.sayHello();
+console.log(user2);
+
+console.log(user2.name);
+console.log(user2.age);
+
+console.log(user2["name"]);
+
+let book = {
+    title: "The King In Yellow",
+    author: "Robert W. Chambers",
+    year: 1895,
+};
+console.log(book.title);
+console.log(book.author);
+console.log(book.year);
+book.year = 1985;
+console.log(book.year);
+
+user2.age = 30;
+user2.name = "Кирилл";
+delete user2.isStudent;
+console.log(user2);
+
+let car = {
+    brand: "",
+    year: 0,
+    getInfo: function () {
+        console.log(`Марка: ${car.brand}, год: ${car.year}`);
+    },
+}
+car.getInfo();
+
+for (let key in user2) {
+    console.log(key + ": " + user2[key]);
 }
 
-for (let value of numbers2) {
-    console.log(value);
+let product = {
+    price: 300,
+    name: "Cup",
+    quality: "Good", 
+};
+for (let key in product) {
+    console.log(key + ": " + product[key]);
 }
 
-let mixedArray = [1, "text", true, 3.14];
-console.log(mixedArray);
+let student = {
+    name: "Григорий",
+    skills: ["HTML", "CSS", "JS"],
+    address: {
+        city: "Волжский",
+        street: "Пушкина",
+    },
+};
 
-console.log(numbersArr.indexOf(1));
-console.log(numbersArr.indexOf(2));
-
-console.log(numbersArr.includes(1));
-console.log(numbersArr.includes(42));
-
-let cities = ["Moscow", "Volgograd", "St.Peterburg"];
-let city = "Volgograd";
-if (cities.includes(city)) {
-    console.log(cities.indexOf(city));
-}
+console.log(student.skills[0]);
+console.log(student.address.city);
